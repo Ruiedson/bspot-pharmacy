b25f5e7800c149669069e85279d71d0d60edc7ee
Ruiedson <ruiedson8@gmail.com>
2026-08-19 15:35:20 +0200

    fix: use Railway DATABASE_URL

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,16 @@
 const mysql = require('mysql2/promise');
 const path = require('path');
+
 require('dotenv').config({
     path: path.resolve(__dirname, '../../.env')
 });
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+// =====================================================
+// CONEXÃO COM MYSQL
+// =====================================================
 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const pool = mysql.createPool(
+    process.env.DATABASE_URL
+);
 
 module.exports = pool;
